6d7a3fabdbd9804592cb2ea22b87bd17816ebbe4
chandler <chandler@sap.com>
2016-06-12 14:40:05 +0800

    first basic route

---
+++ app.js
@@ -0,0 +1,97 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var port = process.env.PORT || 3000
+var app = new express()
+var path = require('path')
+
+app.set('views', './views/pages')
+app.set('view engine','jade')
+app.use(bodyParser.urlencoded())
+app.use(express.static(path.join(__dirname,'bower_components')))
+app.listen(port)
+
+console.log('project has been started on port : ' + port)
+
+
+//index page
+app.get('/', function(req,res){
+	res.render('index',{
+		title: '首页',
+		movies: [{
+			title:'机械战警',
+			_id:1,
+			poster:"http://r3.ykimg.com/05160000530EEB63675839160D0B79D5"
+		},{
+			title:'机械战警',
+			_id:2,
+			poster:"http://r3.ykimg.com/05160000530EEB63675839160D0B79D5"
+		},{
+			title:'机械战警',
+			_id:3,
+			poster:"http://r3.ykimg.com/05160000530EEB63675839160D0B79D5"
+		},{
+			title:'机械战警',
+			_id:4,
+			poster:"http://r3.ykimg.com/05160000530EEB63675839160D0B79D5"
+		},{
+			title:'机械战警',
+			_id:5,
+			poster:"http://r3.ykimg.com/05160000530EEB63675839160D0B79D5"
+		},{
+			title:'机械战警',
+			_id:6,
+			poster:"http://r3.ykimg.com/05160000530EEB63675839160D0B79D5"
+		}]
+	})
+})
+
+//list page
+app.get('/admin/list', function(req,res){
+	res.render('list',{
+		title: '列表',
+		movies: [{
+			doctor:'何塞·帕迪里亚',
+			country: '美国',
+			language: '英语',
+			title: '机械战警',
+			year: 2014,
+			flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+			summary:'2028年，专事军火开发的机器人公司Omni Corp生产了大量装备精良的机械战警，他们被投入到维和和惩治犯罪等行动中，取得显著的效果。罪犯横行的底特律市，嫉恶如仇、正义感十足的警察亚历克斯·墨菲（乔尔·金纳曼饰）遭到仇家暗算，身体受到毁灭性破坏。借助于Omni公司天才博士丹尼特·诺顿（加里·奥德曼饰）最前沿的技术，墨菲以机械战警的形态复活。数轮严格的测试表明，墨菲足以承担起维护社会治安的重任，他的口碑在民众中直线飙升，而墨菲的妻子克拉拉（艾比·考尼什饰）和儿子大卫却再难从他身上感觉亲人的温暖。感知到妻儿的痛苦，墨菲决心向策划杀害自己的犯罪头子展开反击。',
+
+		}]
+	})
+})
+
+//detail page
+app.get('/movie/:id', function(req,res){
+	res.render('detail',{
+		title: '详情',
+		movie: {
+			doctor:'何塞·帕迪里亚',
+			country: '美国',
+			language: '英语',
+			title: '机械战警',
+			year: 2014,
+			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+			flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+			summary:'2028年，专事军火开发的机器人公司Omni Corp生产了大量装备精良的机械战警，他们被投入到维和和惩治犯罪等行动中，取得显著的效果。罪犯横行的底特律市，嫉恶如仇、正义感十足的警察亚历克斯·墨菲（乔尔·金纳曼饰）遭到仇家暗算，身体受到毁灭性破坏。借助于Omni公司天才博士丹尼特·诺顿（加里·奥德曼饰）最前沿的技术，墨菲以机械战警的形态复活。数轮严格的测试表明，墨菲足以承担起维护社会治安的重任，他的口碑在民众中直线飙升，而墨菲的妻子克拉拉（艾比·考尼什饰）和儿子大卫却再难从他身上感觉亲人的温暖。感知到妻儿的痛苦，墨菲决心向策划杀害自己的犯罪头子展开反击。'
+		}
+	})
+})
+
+//admin page
+app.get('/admin/movie', function(req,res){
+	res.render('admin',{
+		title: '管理',
+		movie: {
+			doctor:'',
+			country: '',
+			language: '',
+			title: '',
+			year: '',
+			poster: '',
+			flash: '',
+			summary:''
+		}
+	})
+})
